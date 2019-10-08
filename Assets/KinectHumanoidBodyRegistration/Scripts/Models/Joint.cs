38eabef6dcea6e59e2e330a39fe5113f5d4dc86a
drumath <2237k.drumath@gmail.com>
2019-10-08 14:35:42 +0900

    :sparkles: JointDataの実装

---
+++ Assets/KinectHumanoidBodyRegistration/Scripts/Models/Joint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KinectHumanoidBodyRegistration
+{
+    public class JointData
+    {
+        public Vector3 Position { get; private set; } = new Vector3();
+        public Quaternion Rotation { get; private set; } = Quaternion.identity;
+
+        public void SetPosition(Vector3 pos)
+        {
+            Position = pos;
+        }
+
+        public void SetRotation(Quaternion rot)
+        {
+            Rotation = rot;
+        }
+    }
+}
