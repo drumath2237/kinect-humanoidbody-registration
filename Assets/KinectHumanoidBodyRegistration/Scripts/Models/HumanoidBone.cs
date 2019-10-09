aa5608574225ceeb9d428dc18edbc7d97301a509
drumath <2237k.drumath@gmail.com>
2019-10-10 00:53:12 +0900

    HumanoidBoneDataクラスの定義

---
+++ Assets/KinectHumanoidBodyRegistration/Scripts/Models/HumanoidBone.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KinectHumanoidBodyRegistration
+{
+    public class HumanoidBoneData
+    {
+        public Vector3 Position { get; private set; }
+        public Quaternion Rotation { get; private set; }
+    }
+}
